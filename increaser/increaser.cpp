d495de518aa75994c349b98706467fd17e893e1d
Tim Lobes <timlobes@gmail.com>
2015-01-13 22:22:32 -0800

    Change NULL ptr value to new nullptr to avoid ambiguity

--- increaser/increaser.cpp
+++ increaser/increaser.cpp
@@ -34,7 +34,7 @@ int main (int argc, const char* argv[]) {
     
     char * cPtr = &x;
     int * iPtr = &num;
-    void * nullPtr = NULL; // For testing the null case
+    void * nullPtr = nullptr; // For testing the null case
     
     std::cout << "char size of " << sizeof(x) << " B and int size of " << sizeof(num) << " B\n";
     
